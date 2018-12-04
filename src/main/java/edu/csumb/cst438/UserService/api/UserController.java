3a6273c57464b2c9a65092851cb02601cb4c2296
Adrian Martinez <adrianmartinez@MacBookAir.local>
2018-12-03 18:47:16 -0800

    fixed cross origin, added balance, verify funds routes

--- src/main/java/edu/csumb/cst438/UserService/api/UserController.java
+++ src/main/java/edu/csumb/cst438/UserService/api/UserController.java
@@ -7,35 +7,79 @@ import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class UserController {
 
+    @CrossOrigin()
+    @GetMapping("/balance/{id}")
+    @ResponseBody
+    int balance(@PathVariable String id) {
+        int bal = dbBalance(id);
+        return bal;
+    }
+
+    private int dbBalance(String id) {
+        String url = "http://localhost:8079/balance/" + id;
+        // Create the request body as a MultiValueMap
+        MultiValueMap<String, String> body = new LinkedMultiValueMap<>();
+        HttpEntity<MultiValueMap<String, String>> httpEntity = new HttpEntity<>(body, null);
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<Integer> result = restTemplate.exchange(
+                url,
+                HttpMethod.GET, null,
+                new ParameterizedTypeReference<Integer>() {
+                });
+        return result.getBody();
+    }
+
+    @CrossOrigin()
+    @GetMapping("/verify/funds/{id}/{amount}")
+    @ResponseBody
+    boolean balance(@PathVariable String id, @PathVariable String amount) {
+        boolean funds = dbVerifyPurchase(id, amount);
+        return funds;
+    }
+
+    private boolean dbVerifyPurchase(String id, String amount) {
+        String url = "http://localhost:8079/verify/funds/" + id + "/" + amount;
+        // Create the request body as a MultiValueMap
+        MultiValueMap<String, String> body = new LinkedMultiValueMap<>();
+        HttpEntity<MultiValueMap<String, String>> httpEntity = new HttpEntity<>(body, null);
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<Boolean> result = restTemplate.exchange(
+                url,
+                HttpMethod.GET, null,
+                new ParameterizedTypeReference<Boolean>() {
+                });
+        System.out.println(result.getBody());
+        return result.getBody();
+    }
+
+    @CrossOrigin()
     @PostMapping("/login")
     @ResponseBody
-    boolean signIn(@RequestParam String username) {
-        boolean authorized = dbLogin(username);
+    String signIn(@RequestParam String username) {
+        String authorized = dbLogin(username);
         return authorized;
     }
 
-    private boolean dbLogin(String username) {
+    private String dbLogin(String username) {
         String url = "http://localhost:8081/login";
         // Create the request body as a MultiValueMap
         MultiValueMap<String, String> body = new LinkedMultiValueMap<>();
         body.add("username", username);
         HttpEntity<MultiValueMap<String, String>> httpEntity = new HttpEntity<>(body, null);
         RestTemplate restTemplate = new RestTemplate();
-        ResponseEntity<Boolean> result = restTemplate.exchange(
+        ResponseEntity<String> result = restTemplate.exchange(
                 url,
                 HttpMethod.POST,
                 httpEntity,
-                new ParameterizedTypeReference<Boolean>() {
+                new ParameterizedTypeReference<String>() {
                 });
+        System.out.println(result);
         return result.getBody();
     }
 
